113caf53031503ca720ff4347f4b6e4139b07cdb
Afoma-tech <156825770+Afoma-tech@users.noreply.github.com>
2024-04-12 00:26:14 -0400

    The unmodified files

--- main.js
+++ main.js
@@ -1,51 +1,18 @@
-// Name: Afoma Egwuatu
-// Date: 11th April, 2024
-// Description: Ensuring Accessibility on a webpage - making it dynamic
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
-// functionality for showing/hiding the comments section
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
 
-const showHideBtn = document.querySelector('.show-hide');
-const commentWrapper = document.querySelector('.comment-wrapper');
+/* Declaring the array of image filenames */
 
-commentWrapper.style.display = 'none';
+/* Declaring the alternative text for each image file */
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
-    showHideBtn.textContent = 'Hide comments';
-    commentWrapper.style.display = 'block';
-  } else {
-    showHideBtn.textContent = 'Show comments';
-    commentWrapper.style.display = 'none';
-  }
-};
+/* Looping through images */
 
-// functionality for adding a new comment via the comments form
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
 
-const form = document.querySelector('.comment-form');
-const nameField = document.querySelector('#name');
-const commentField = document.querySelector('#comment');
-const list = document.querySelector('.comment-container');
-
-form.onsubmit = function(e) {
-  e.preventDefault();
-  submitComment();
-};
-
-function submitComment() {
-  const listItem = document.createElement('li');
-  const namePara = document.createElement('p');
-  const commentPara = document.createElement('p');
-  const nameValue = nameField.value;
-  const commentValue = commentField.value;
-
-  namePara.textContent = nameValue;
-  commentPara.textContent = commentValue;
-
-  list.appendChild(listItem);
-  listItem.appendChild(namePara);
-  listItem.appendChild(commentPara);
-
-  nameField.value = '';
-  commentField.value = '';
-}
+/* Wiring up the Darken/Lighten button */
