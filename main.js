0f9c6f428a2947643284a1281333d0047734104c
Afoma-tech <156825770+Afoma-tech@users.noreply.github.com>
2024-04-12 00:41:51 -0400

    alt text has been declared for each file

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = {
+    'pic1.jpg': 'Closeup of a blue human eye',
+    'pic2.jpg': 'A beautiful landscape',
+    'pic3.jpg': 'A cute animal',
+    'pic4.jpg': 'Abstract art',
+    'pic5.jpg': 'City skyline at night'
+};
 
 /* Looping through images */
+imageFilenames.forEach
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
